5198ec94f7158b547d0a5983190341d191833cec
Geoffdl <geoffroydelorme@gmail.com>
2025-03-25 21:01:33 +0100

    equals

---
+++ src/main/java/fr/diginamic/equals/Pays.java
@@ -0,0 +1,50 @@
+package fr.diginamic.equals;
+
+import java.util.StringJoiner;
+
+public class Pays {
+    public String nom;
+    public int population;
+
+    public Pays(String nom, int population) {
+        this.nom = nom;
+        this.population = population;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Pays.class.getSimpleName() + "[", "]")
+                .add("nom='" + nom + "'")
+                .add("population=" + population)
+                .toString();
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Pays pays = (Pays) obj;
+        return population == pays.population &&
+                nom.equals(pays.nom);
+    }
+
+}
---
+++ src/main/java/fr/diginamic/equals/TestEqualsPays.java
@@ -0,0 +1,26 @@
+package fr.diginamic.equals;
+
+import java.util.Objects;
+
+public class TestEqualsPays {
+    public static void main(String[] args) {
+        Pays p = new Pays("Tinytopie", 2);
+        Pays p2 = new Pays("Olalaland", 2);
+
+        boolean tomate = p.equals(p2);
+        System.out.println(tomate);
+    /*
+        if (Objects.equals(p.nom, p2.nom)) {
+            System.out.println("name match");
+        } else {
+            System.out.println("name no match");
+        }
+        if (p.population == p2.population) {
+            System.out.println("==");
+        } else {
+            System.out.println("pop no match");
+        }
+
+     */
+    }
+}
